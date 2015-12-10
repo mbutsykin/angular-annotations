0cf42e5e0ec16bc1fa1609e8db638b9eb4faa8bd
Maksym Butsykin <mbutsykin@gmail.com>
2015-12-10 18:30:56 +0100

    config decorator

--- src/main/ES/annotations/config.js
+++ src/main/ES/annotations/config.js
@@ -1,7 +1,19 @@
+import angular from 'angular';
+import {getInjectableClass} from 'util/helpers';
+
 /**
  * Register work which needs to be performed on module loading
+ *
+ * @param params
+ * @returns {Function}
  */
-function config() {
+function config(params) {
+    return target => {
+        angular.module(params.module)
+            .config(getInjectableClass(target));
+
+        return target;
+    };
 }
 
 export {config};
