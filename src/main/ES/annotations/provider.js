55d1a4c36a41a61e19ad0871c3a9a5846f153563
Maksym Butsykin <mbutsykin@gmail.com>
2015-12-10 18:08:13 +0100

    provider decorator

--- src/main/ES/annotations/provider.js
+++ src/main/ES/annotations/provider.js
@@ -1,7 +1,14 @@
+import angular from 'angular';
+
 /**
  * Register a provider function
  */
-function provider() {
+function provider(params) {
+    return target => {
+        angular.module(params.module).provider(target.name, target);
+
+        return target;
+    }
 }
 
 export {provider};
