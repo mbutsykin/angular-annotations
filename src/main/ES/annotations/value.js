636b7ac25a63722e470162758d67367f43dede10
Maksym Butsykin <mbutsykin@gmail.com>
2015-12-10 18:12:50 +0100

    value decorator

--- src/main/ES/annotations/value.js
+++ src/main/ES/annotations/value.js
@@ -1,7 +1,18 @@
+import angular from 'angular';
+
 /**
  * Register a value service
+ *
+ * @param params
+ * @returns {Function}
  */
-function value() {
+function value(params) {
+    return target => {
+        angular.module(params.module)
+            .constant(target.name, new target());
+
+        return target;
+    };
 }
 
 export {value};
