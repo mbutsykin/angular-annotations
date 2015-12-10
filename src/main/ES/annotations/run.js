6035a160290ca328a6f771b236e53b8f45a8646e
Maksym Butsykin <mbutsykin@gmail.com>
2015-12-10 18:32:11 +0100

    run decorator

--- src/main/ES/annotations/run.js
+++ src/main/ES/annotations/run.js
@@ -1,7 +1,19 @@
+import angular from 'angular';
+import {getInjectableClass} from 'util/helpers';
+
 /**
  * Register work which should be performed when the injector is done loading all modules
+ *
+ * @param params
+ * @returns {Function}
  */
-function run() {
+function run(params) {
+    return target => {
+        angular.module(params.module)
+            .run(getInjectableClass(target));
+
+        return target;
+    };
 }
 
 export {run};
