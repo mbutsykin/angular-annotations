a4ac86d439d88f9a2ffd8e3c74a033568b24b815
Maksym Butsykin <mbutsykin@gmail.com>
2015-12-10 18:29:02 +0100

    directive decorator

--- src/main/ES/annotations/directive.js
+++ src/main/ES/annotations/directive.js
@@ -1,7 +1,19 @@
+import angular from 'angular';
+import {getInjectableClass, pascalCaseToCamelCase} from 'util/helpers';
+
 /**
  * Register a directive
+ *
+ * @param params
+ * @returns {Function}
  */
-function directive() {
+function directive(params) {
+    return target => {
+        angular.module(params.module)
+            .directive(pascalCaseToCamelCase(params.selector || target.name), new target());
+
+        return target;
+    };
 }
 
 export {directive};
