82ca0af8d26c5cc42495f94bb0b4eb7b17f08528
Maksym Butsykin <mbutsykin@gmail.com>
2015-12-10 19:26:25 +0100

    @Controller test

---
+++ src/test/ES/annotations/controller.spec.js
@@ -0,0 +1,19 @@
+import {Controller} from 'bootstrap';
+
+describe('@Controller decorator', () => {
+    const module = angular.module('test', []);
+
+    @Controller({module: 'test'})
+    class SomeController {
+    }
+
+    it('Controller was added', () => {
+        expect(module._invokeQueue[module._invokeQueue.length - 1][1]).toBe('register');
+    });
+
+    it('Controller name correct', () => {
+        var controller = module._invokeQueue[module._invokeQueue.length - 1][2];
+
+        expect(controller[0]).toBe('SomeController');
+    });
+});
