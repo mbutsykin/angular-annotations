8218bbb59c4f344dcc26388ff9ab16203579fd96
Maksym Butsykin <mbutsykin@gmail.com>
2015-12-10 18:11:36 +0100

    service decorator

--- src/main/ES/annotations/service.js
+++ src/main/ES/annotations/service.js
@@ -1,7 +1,18 @@
+import angular from 'angular';
+
 /**
  * Register a service constructor
+ *
+ * @param params
+ * @returns {Function}
  */
-function service() {
+function service(params) {
+    return target => {
+        angular.module(params.module)
+            .service(target.name, target);
+
+        return target;
+    };
 }
 
 export {service};
