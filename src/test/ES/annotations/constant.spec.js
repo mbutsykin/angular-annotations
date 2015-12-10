009cde8fcc9537137900c8c8d24f19ebca906eef
Maksym Butsykin <mbutsykin@gmail.com>
2015-12-10 19:23:42 +0100

    @Constant test

---
+++ src/test/ES/annotations/constant.spec.js
@@ -0,0 +1,21 @@
+import {Constant} from 'bootstrap';
+
+describe('@Constant decorator', () => {
+    const module = angular.module('test', []);
+    const constantName = 'CONSTANT';
+
+    @Constant({module: 'test', name: constantName})
+    class SomeConfig {
+        test = 1;
+    }
+
+    it('Constant was added', () => {
+        expect(module._invokeQueue[module._invokeQueue.length - 1][1]).toBe('constant');
+    });
+
+    it('Constant name correct', () => {
+        var constant = module._invokeQueue[module._invokeQueue.length - 1][2];
+
+        expect(constant[0]).toBe(constantName);
+    });
+});
