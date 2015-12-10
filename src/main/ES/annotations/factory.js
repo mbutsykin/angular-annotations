8b29481a3cce586ca39333c99098b4d74a2d2d68
Maksym Butsykin <mbutsykin@gmail.com>
2015-12-10 18:10:13 +0100

    factory decorator

--- src/main/ES/annotations/factory.js
+++ src/main/ES/annotations/factory.js
@@ -1,7 +1,16 @@
+import angular from 'angular';
+import {getInjectableClass} from 'util/helpers';
+
 /**
  * Register a service factory
  */
-function factory() {
+function factory(params) {
+    return target => {
+        angular.module(params.module)
+            .factory(target.name, getInjectableClass(target));
+
+        return target;
+    };
 }
 
 export {factory};
