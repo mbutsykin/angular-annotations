bcad5978e69f874c91d0bf7ef81a29731361baba
Maksym Butsykin <mbutsykin@gmail.com>
2015-12-10 18:17:06 +0100

    decorator decorator

--- src/main/ES/annotations/decorator.js
+++ src/main/ES/annotations/decorator.js
@@ -1,7 +1,19 @@
+import angular from 'angular';
+import {getInjectableClass} from 'util/helpers';
+
 /**
  * Register a service decorator
+ *
+ * @param params
+ * @returns {Function}
  */
-function decorator() {
+function decorator(params) {
+    return target => {
+        angular.module(params.module)
+            .decorator(params.name, getInjectableClass(target));
+
+        return target;
+    };
 }
 
 export {decorator};
