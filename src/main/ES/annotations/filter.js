4e4004c91457b99646336e745c93fe7e4e349cce
Maksym Butsykin <mbutsykin@gmail.com>
2015-12-10 18:19:39 +0100

    filter decorator

--- src/main/ES/annotations/filter.js
+++ src/main/ES/annotations/filter.js
@@ -1,7 +1,16 @@
+import angular from 'angular';
+import {getInjectableClass} from 'util/helpers';
+
 /**
  * Register a filter
  */
-function filter() {
+function filter(params) {
+    return target => {
+        angular.module(params.module)
+            .filter(params.name || target.name, getInjectableClass(target));
+
+        return target;
+    };
 }
 
 export {filter};
